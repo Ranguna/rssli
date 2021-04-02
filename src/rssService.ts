95ab3a792cde912d3e828bf035b2ac3575822dd3
Ranguna <luis.pais@pm.me>
2021-04-02 22:55:15 +0100

    Fixed typing.

--- src/rssService.ts
+++ src/rssService.ts
@@ -3,7 +3,7 @@ import {RSSFeedForLinks} from "./rssService.entity";
 import {FeedItem} from "./feed.entity";
 export const parser = new Parser();
 
-export const getTitlesFromRssItems = (rssItems: any[]) => rssItems.map((item: any) => item.title);
+export const getTitlesFromRssItems = (rssItems: Item[]) => rssItems.map((item: Item) => item.title);
 
 export const sortRssItemsByDate = (rssItems: Item[], direction = 1): FeedItem[] =>
   rssItems.map(item => ({
